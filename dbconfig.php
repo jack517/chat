cbfcdb3dcfd88de1e1459c4286ef970d0abec12c
jack517 <mg99673@gmail.com>
2017-04-10 16:35:47 +0530

    Delete dbconfig.php

--- dbconfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db_hostname = "localhost";
-$db_username = "root";
-$db_password = "";
-$db_database="hacka";
-$connection = new mysqli($db_hostname,$db_username,$db_password,$db_database);
-
-if($connection->connect_error) die($connection->connect_error);
-?>
